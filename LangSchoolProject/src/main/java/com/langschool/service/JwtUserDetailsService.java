706ea4350e2b4f1f83092d10b5b4c87acd3f63f4
mBadurczyk <michal.badurczyk@gmail.com>
2021-06-22 19:06:22 +0200

    Minor changes to code

--- LangSchoolProject/src/main/java/com/langschool/service/JwtUserDetailsService.java
+++ LangSchoolProject/src/main/java/com/langschool/service/JwtUserDetailsService.java
@@ -5,7 +5,6 @@ package com.langschool.service;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
